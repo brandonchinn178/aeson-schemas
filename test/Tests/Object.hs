7b66281c96cb0c709af43fe744b5bb5256f14ff8
Brandon Chinn <brandon@leapyear.io>
2020-07-20 12:24:05 -0700

    Add tests for FromJSON (Object schema) instance

--- test/Tests/Object.hs
+++ test/Tests/Object.hs
@@ -2,9 +2,11 @@ module Tests.Object where
 
 import Test.Tasty
 
+import qualified Tests.Object.FromJSON
 import qualified Tests.Object.Show
 
 test :: TestTree
 test = testGroup "Object"
   [ Tests.Object.Show.test
+  , Tests.Object.FromJSON.test
   ]
