0af1d4e961e3af657bbb8e89801e857eeaaaa62b
Brandon Chinn <brandon@leapyear.io>
2020-07-22 12:59:31 -0700

    Add toMap test

--- test/Tests/Object.hs
+++ test/Tests/Object.hs
@@ -1,12 +1,29 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+
 module Tests.Object where
 
+import Data.Aeson (Value(..))
+import Data.Aeson.QQ (aesonQQ)
+import qualified Data.HashMap.Lazy as HashMap
 import Test.Tasty
+import Test.Tasty.HUnit
 
+import Data.Aeson.Schema (Object, schema, toMap)
 import qualified Tests.Object.FromJSON
 import qualified Tests.Object.Show
+import TestUtils (parseValue)
 
 test :: TestTree
 test = testGroup "Object"
   [ Tests.Object.Show.test
   , Tests.Object.FromJSON.test
+
+  , testCase "toMap smoketest" $
+      let o :: Object [schema| { a: Bool } |]
+          o = parseValue [aesonQQ| { "a": true } |]
+      in toMap o @?= HashMap.fromList
+        [ ("a", Bool True)
+        ]
   ]
