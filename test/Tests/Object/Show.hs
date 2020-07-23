d535ee909435c52b91b6bf8d76f8ec050fd43398
Brandon Chinn <brandon@leapyear.io>
2020-07-23 15:01:23 -0700

    Reduce test cases due to SchemaScalar consolidation

--- test/Tests/Object/Show.hs
+++ test/Tests/Object/Show.hs
@@ -1,12 +1,10 @@
 {-# LANGUAGE DataKinds #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TemplateHaskell #-}
 
 module Tests.Object.Show where
 
-import Data.Aeson (FromJSON, ToJSON(..))
 import Data.Aeson.QQ (aesonQQ)
 import Data.String.Interpolate (i)
 import Test.Tasty
@@ -16,45 +14,26 @@ import Test.Tasty.QuickCheck
 import Tests.Object.Show.TH
 import TestUtils (parseObject)
 
-newtype Coordinate = Coordinate (Int, Int)
-  deriving (Show,Eq,ToJSON,FromJSON,Arbitrary)
-
 test :: TestTree
 test = testGroup "Show instance"
-  [ testProperty "Bool key" $ \(b :: Bool) ->
-      let o = $(parseObject "{ foo: Bool }") [aesonQQ| { "foo": #{b} } |]
-      in show o === [i|{"foo": #{show b}}|]
-
-  , testProperty "Int key" $ \(x :: Int) ->
-      let o = $(parseObject "{ foo: Int }") [aesonQQ| { "foo": #{x} } |]
-      in show o === [i|{"foo": #{show x}}|]
-
-  , testProperty "Double key" $ \(x :: Double) ->
-      let o = $(parseObject "{ foo: Double }") [aesonQQ| { "foo": #{x} } |]
-      in show o === [i|{"foo": #{show x}}|]
-
-  , testProperty "Text key" $ \(s :: String) ->
+  [ testProperty "Scalar key" $ \(s :: String) ->
       let o = $(parseObject "{ foo: Text }") [aesonQQ| { "foo": #{s} } |]
       in show o === [i|{"foo": #{show s}}|]
 
-  , testProperty "Custom key" $ \(c :: Coordinate) ->
-      let o = $(parseObject "{ foo: Coordinate }") [aesonQQ| { "foo": #{c} } |]
-      in show o === [i|{"foo": #{show c}}|]
-
-  , testProperty "Object with multiple keys" $ \(b :: Bool, x :: Int) ->
-      let o = $(parseObject "{ foo: Bool, bar: Int }") [aesonQQ| { "foo": #{b}, "bar": #{x} } |]
+  , testProperty "Object with multiple keys" $ \(b :: Double, x :: Int) ->
+      let o = $(parseObject "{ foo: Double, bar: Int }") [aesonQQ| { "foo": #{b}, "bar": #{x} } |]
       in show o === [i|{"foo": #{show b}, "bar": #{show x}}|]
 
   , testProperty "Nested object" $ \(x :: Int) ->
       let o = $(parseObject "{ foo: { bar: Int } }") [aesonQQ| { "foo": { "bar": #{x} } } |]
       in show o === [i|{"foo": {"bar": #{show x}}}|]
 
-  , testProperty "Object with existing Maybe key" $ \(b :: Bool) ->
-      let o = $(parseObject "{ foo: Maybe Bool }") [aesonQQ| { "foo": #{b} } |]
-      in show o === [i|{"foo": Just #{show b}}|]
+  , testProperty "Object with existing Maybe key" $ \(x :: Bool) ->
+      let o = $(parseObject "{ foo: Maybe Bool }") [aesonQQ| { "foo": #{x} } |]
+      in show o === [i|{"foo": Just #{show x}}|]
 
   , testCase "Object with non-existing Maybe key" $
-      let o = $(parseObject "{ foo: Maybe Bool }") [aesonQQ| { "foo": null } |]
+      let o = $(parseObject "{ foo: Maybe Double }") [aesonQQ| { "foo": null } |]
       in show o @?= [i|{"foo": Nothing}|]
 
   , testProperty "Object with valid Try key" $ \(b :: Bool) ->
