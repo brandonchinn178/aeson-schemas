33c11174d3887db2ea643124eb470b4502e93895
Brandon Chinn <brandon@leapyear.io>
2020-09-02 11:15:08 -0700

    Refactor benchmarks

--- bench/Schemas.hs
+++ bench/Schemas.hs
@@ -1,19 +1,40 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TemplateHaskell #-}
+
 module Schemas
-  ( genSchema
-  , genSchemaDef
+  ( module Schemas
+  , module Schemas.TH
   ) where
 
-import Data.List (intercalate)
+import Control.Monad (forM)
+import Data.Char (chr, toUpper)
 import Language.Haskell.TH
-import Language.Haskell.TH.Quote
+import Language.Haskell.TH.Syntax (lift)
+import Language.Haskell.TH.TestUtils ()
 
-import Data.Aeson.Schema (schema)
+import Schemas.TH
 
-genSchema :: Int -> TypeQ
-genSchema = quoteType schema . genSchemaDef
+-- Generates:
+--
+--   type Schema1   = { a1: Int }
+--   type Schema5   = { a1: Int, a2: Int, ... a5: Int }
+--   type Schema10  = { a1: Int, a2: Int, ... a10: Int }
+--   type Schema100 = { a1: Int, a2: Int, ... a100: Int }
+$(do
+  -- The sizes of schemas to generate
+  let schemaSizes = [1, 5, 10, 100]
+      allSchemas = flip map schemaSizes $ \n ->
+        let name = "Schema" ++ show n
+        in (n, name, mkName name)
 
-genSchemaDef :: Int -> String
-genSchemaDef n = "{" ++ intercalate "," (map mkField [1..n]) ++ "}"
-  where
-    mkField i = "a" ++ show i ++ ": Int"
+  concat <$> sequence
+    [ forM allSchemas $ \(n, _, name) -> genSchema name $ keysTo n
+    , [d|
+        sizedSchemas :: [(Int, String)]
+        sizedSchemas = $(lift $ flip map allSchemas $ \(n, name, _) -> (n, name))
 
+        sizedSchemasNames :: [(String, Name)]
+        sizedSchemasNames = $(lift $ flip map allSchemas $ \(_, name, thName) -> (name, thName))
+      |]
+    ]
+  )
